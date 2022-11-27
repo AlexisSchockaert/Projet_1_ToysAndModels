b3983be77f8c9f7dd9ad344cbeaa2fd85c6a4d8d
Alexis Schockaert <104074573+AlexisSchockaert@users.noreply.github.com>
2022-11-27 11:04:14 +0100

    Delete LOGISTIQUE directory

--- LOGISTIQUE/LOG_Délai moyen de préparation.sql
+++
@@ -1,7 +0,0 @@
--- ----------------REQUETE POWER-BI---------------
-
--- délai préparation en jour calendaire (Date Expédition - Date commande)
--- calcule la moyenne pour toutes les préparations de l'année en cours
-SELECT AVG(DATEDIFF(shippedDate,orderDate))
-FROM orders
-WHERE YEAR(orderdate)=YEAR(NOW()) AND shippeddate IS NOT NULL;
--- LOGISTIQUE/LOG_Montant du stock.sql
+++
@@ -1,4 +0,0 @@
--- --------------------REQUETE POWER-BI----------------
--- Calcule le montant du stock = qté * prix d'achat pour tous les produits
-Select Sum(products.quantityinstock * buyprice) as Valeur_Stock_Global
-FROM Products;
